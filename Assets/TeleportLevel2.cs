a7798ff77ce88832a4aab1bbb3bd1c758f09631c
Nav <navya.suri13@gmail.com>
2018-12-10 21:51:14 +0400

    Final changes made, Diverge changed

---
+++ Assets/TeleportLevel2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportLevel2 : MonoBehaviour {
+
+	public string teleportTo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collider col){
+		if (col.tag == "Player") {
+			teleport ();
+		}
+	}
+
+	IEnumerator teleport(){
+		yield return new WaitForSeconds (5f);
+		SceneManager.LoadScene (teleportTo);
+	}
+}
